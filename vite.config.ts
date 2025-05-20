3964e61adaf956761c12840b3f7703aab8b2d435
Alex Zenchik <alexcomp2021@gmail.com>
2025-05-20 08:26:58 +0300

    refactor: some changes in packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [react(), tailwindcss(), viteStaticCopy({targets: [{ src: "public/assets", dest: "assets" }]})],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
+	},
+	build: {
+		outDir: "dist",
 	},
 });
